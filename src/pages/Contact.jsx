682d81f1dc0b4311481ab38749a098799d0b2f80
Gedeon <fobadeffo@gmail.com>
2025-10-29 01:19:48 +0100

    Google adds

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -46,7 +46,7 @@ export default function Contact() {
 
         {/* Mobile */}
         {isOpen && (
-                    <div className="md:hidden bg-white shadow-lg border-t border-gray-100">
+                    <div className="md:hidden bg-white shadow-lg border-t border-gray-100 fixed top-0 left-0 min-h-[100vh] min-w-[300px]">
                         <div className="flex flex-col px-6 py-4 space-y-4 font-medium text-gray-700">
                         <Link
                             to="/"
